f3023cf87e055103e7e44f999ee4674453625423
arcthomas <bertoluzzit2@gmail.com>
2024-12-12 23:46:22 -0300

    Commit 2

--- Controllers/PresupuestosController.cs
+++ Controllers/PresupuestosController.cs
@@ -8,11 +8,26 @@ public class PresupuestosController : Controller
         return View();
     }
     [HttpGet]
-    public  IActionResult Listar()
+    public IActionResult Listar()
     {
         PresupuestosRepository repo = new PresupuestosRepository();
         List<Presupuestos> listaPres = new List<Presupuestos>();
         listaPres = repo.GetAll();
         return View(listaPres);
+    }
+    [HttpGet]
+    public IActionResult Modificar()
+    {
+        PresupuestosRepository repo = new PresupuestosRepository();
+        List<Presupuestos> listaPres = new List<Presupuestos>();
+        listaPres = repo.GetAll();
+        return View(listaPres);
+    }
+    [HttpPost]
+    public IActionResult ModificarOk(Presupuestos presupuesto)
+    {
+        PresupuestosRepository repo = new PresupuestosRepository();
+        repo.Update(presupuesto);
+        return RedirectToAction("Modificar");
     }
 }
